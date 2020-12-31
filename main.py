d36589c1243157ab4175402a5a3ffb70bc9d4631
ekalle <ekalle@ttu.ee>
2020-12-31 18:19:03 +0200

    1st

--- main.py
+++ main.py
@@ -19,44 +19,59 @@ class Sudoku:
 
     def make_coords(self):
         coords = {}
-        for i in self.board:
+        for height in self.board:
             # fix if not right len
-            while len(self.board[i]) != 9:
-                self.board[i] += " "
-            for j in range(len(self.board[i])):
-                name = f"{i}{j}"
-                if i in range(0, 3):
+            while len(self.board[height]) < 9:
+                self.board[height] += " "
+            for lenght in range(len(self.board[height])):
+                name = f"{height}{lenght}"
+                if height in range(0, 3):
                     b1 = 0
-                elif i in range(3,6):
+                elif height in range(3,6):
                     b1 = 1
                 else:
                     b1 = 2
-                if j in range(0, 3):
+                if lenght in range(0, 3):
                     b2 = 0
-                elif j in range(3,6):
+                elif lenght in range(3,6):
                     b2 = 1
                 else:
                     b2 = 2
-                if self.board[i][j] == " ":
-                    coords[name] = (set(), f"{b1}{b2}")
+                if self.board[height][lenght] == " ":
+                    coords[name] = set(), f"{b1}{b2}"
                 else:
-                    coords[name] = (int(self.board[i][j]), f"{b1}{b2}")
+                    coords[name] = int(self.board[height][lenght]), f"{b1}{b2}"
         return coords
 
-    def show_board(self):
-        file = ""
-        for i in self.board:
-            for j in range(len(self.board[i])):
-                name = f"{i}{j}"
-                if type(self.coords[name][0]) == int:
-                    file += str(self.coords[name][0])
-                else:
-                    file += " "
-            file += "\n"
+    def show_board(self, coord=False, reason=None):
+        if coord:
+            h = int(coord[0])
+            l = int(coord[1])
+        else:
+            h = None
+            l = None
+        file = str()
+        key = self.board.keys()
+        for i in range(0, len(key), 3):
+            part = {}
+            for j in range(3):
+                k = i + j
+                line = self.board[k]
+                p1 = line[:3]
+                p2 = line[3:][:3]
+                p3 = line[6:][:3]
+                part[j] = f"{p1}|{p2}|{p3}"
+                if k == h:
+                    number = self.board[h][l]
+                    part[j] += f" <---({number}) ({reason})"
+            file += part[0] + "\n" + part[1] + "\n" + part[2]
+            if i == 0 or i == 3:
+                file += "\n---+---+---\n"
         return file
 
     def mark(self):
-        """Marks what coord cant be"""
+        """Marks coord with values of other boxes near it what it cant be"""
+        # checks hor/ver lines
         for i in self.coords:
             # print(self.coords[i])
             if type(self.coords[i][0]) == int:
@@ -70,8 +85,8 @@ class Sudoku:
                     new = f"{y}{j}"
                     if type(self.coords[new][0]) == set:
                         self.coords[new][0].add(self.coords[i][0])
+        # checks its 3x3 box values
         a = self.coords.items()
-        # sorts list by box coors so 0-8 is first box
         b = sorted(a, key=lambda x: x[1][1])
         for i in range(0, len(b), 9):
             n = 0
@@ -89,32 +104,49 @@ class Sudoku:
 
     def solve(self):
         """Uses other algorithms."""
-        self.mark()
-        print(self.show_board())
-        time.sleep(1)
-        change = self.lenght()
-        if change:
-            self.solve()
-        else:
-            change2 = self.box()
-            if change2:
-                self.solve()
-        # return self.show_board()
+        change = True
+        number = False
+        while change:
+            time.sleep(0.1)
+            print(self.show_board(coord=number))
+            print()
+            self.mark()
+            some = self.lenght()
+            change = some[0]
+            number = some[1]
+            reason = some
+            if change:
+                continue
+            else:
+                change = self.box()
+                if change:
+                    some = self.lenght()
+                    number = some[1]
+                    continue
+                else:
+                    change = self.multi()
+                    some = self.lenght()
+                    number = some[1]
 
     def lenght(self):
         """solve if only 1 number can go into box"""
-        numbers = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
-        change = False
+        numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9}
         for i in self.coords:
-            # print(self.coords[i])
             if type(self.coords[i][0]) == set:
                 if len(self.coords[i][0]) == 8:
-                    change = True
-                    self.coords[i] = (list(numbers - self.coords[i][0])[0], self.coords[i][1])
-        return change
+                    number = list(numbers - self.coords[i][0])[0]
+                    self.coords[i] = (number, self.coords[i][1])
+                    h = int(i[0])
+                    l = int(i[1])
+                    lista = list(self.board[h])
+                    lista[l] = str(number)
+                    line = "".join(lista)
+                    self.board[h] = line
+                    return True, i
+        return False, None
 
     def box(self):
-        """up box's value by checking other box's"""
+        """solve 3x3 if a number can only be in 1 place"""
         a = self.coords.items()
         # sorts list by box coors so 0-8 is first box
         b = sorted(a, key=lambda x: x[1][1])
@@ -122,107 +154,107 @@ class Sudoku:
         for i in range(0, len(b), 9):
             n = 0
             numbers = set()
-            all_numbers = set([1,2,3,4,5,6,7,8,9])
+            all_numbers = {1,2,3,4,5,6,7,8,9}
             while n < 9:
                 if type(b[i + n][1][0]) == int:
                     numbers.add(b[i + n][1][0])
                 n += 1
-            all_numbers -= numbers
+            remain = list(all_numbers - numbers)
             sets = []
             n = 0
             while n < 9:
                 if type(b[i + n][1][0]) == set:
                     sets.append((b[i + n][1][0], b[i + n][0]))
                 n += 1
-            for j in all_numbers:
-                value = []
+            needed = len(remain) - 1
+            for j in remain:
+                count = 0
                 for k in sets:
-                    if j not in k[0]:
-                        value.append(k)
-                if len(value) == 1:
+                    if j in k[0]:
+                        count += 1
+                    else:
+                        value = k
+                if needed == count:
+                    # print(value[1], j)
+                    values = all_numbers - {j}
+                    self.coords[value[1]] = (values, self.coords[value[1]][1])
                     change = True
-                    self.coords[value[0][1]] = (j, self.coords[value[0][1]][1])
         return change
 
-
-
-    # def box(self):
-    #     keys = list(self.coords.keys())
-    #     some_list = []
-    #     for i in range(0, len(keys), 3):
-    #         if keys[i][0] in ["0", "3", "6"]:
-    #             box = []
-    #             a = keys[i]
-    #             b = keys[i+1]
-    #             c = keys[i+2]
-    #             d = str(int(keys[i][0])+1) + keys[i][1]
-    #             e = str(int(keys[i][0])+1) + keys[i+1][1]
-    #             f = str(int(keys[i][0])+1) + keys[i+2][1]
-    #             g = str(int(keys[i][0])+2) + keys[i][1]
-    #             h = str(int(keys[i][0])+2) + keys[i+1][1]
-    #             i = str(int(keys[i][0])+2) + keys[i+2][1]
-    #             box.extend((a, b, c, d, e, f, g, h, i))
-    #             some_list.append(box)
-    #     return some_list
-    #
-    # def box_box(self):
-    #     some_list = self.box()
-    #     change = False
-    #     for box in some_list:
-    #         numbers = []
-    #         for j in box:
-    #             if type(self.coords[j]) == int:
-    #                 numbers.append(self.coords[j])
-    #         for j in box:
-    #             if type(self.coords[j]) == list:
-    #                 for k in numbers:
-    #                     if k not in self.coords[j]:
-    #                         change = True
-    #                         self.coords[j].append(k)
-    #     return change
-    #
-    # def box_one_position(self):
-    #     some_list = self.box()
-    #     all_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    #     change = False
-    #     for box in some_list:
-    #         numbers = []
-    #         lists = []
-    #         for j in box:
-    #             if type(self.coords[j]) == int:
-    #                 numbers.append(self.coords[j])
-    #         a = list(set(all_numbers) - set(numbers))
-    #         for j in box:
-    #             if type(self.coords[j]) == list:
-    #                 lists.append((self.coords[j], j))
-    #         for j in a:
-    #             places = []
-    #             for k in lists:
-    #                 if j not in k[0]:
-    #                     places.append(k[1])
-    #             if len(places) == 1:
-    #                 change = True
-    #                 # print(self.coords[places[0]], j)
-    #                 self.coords[places[0]] = j
-    #     return change
+    def multi(self):
+        """
+        update other boxes if 2 or 3 boxes can only have certain numbers
+        for example [123][123][123]|[1234] then 4th box is 4 as 123 can act as permanent values
+        """
+        a = self.coords.items()
+        # sorts list by box coors so 0-8 is first box
+        b = sorted(a, key=lambda x: x[1][1])
+        change = False
+        for i in range(0, len(b), 9):
+            n = 0
+            numbers = set()
+            all_numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+            while n < 9:
+                if type(b[i + n][1][0]) == int:
+                    numbers.add(b[i + n][1][0])
+                n += 1
+            remain = list(all_numbers - numbers)
+            sets = []
+            n = 0
+            while n < 9:
+                if type(b[i + n][1][0]) == set:
+                    sets.append((b[i + n][1][0], b[i + n][0]))
+                n += 1
+            # print(numbers, remain)
+            # print(sets)
+            # if 2 in a row
+            rows = []
+            for j in remain:
+                coords = set()
+                for k in sets:
+                    # print(j, k[0], count)
+                    if j not in k[0]:
+                        coords.add(k[1])
+                # print(count)
+                if len(coords) == 2:
+                    rows.append((coords,j))
+            for j in rows:
+                for k in rows:
+                    # print(j)
+                    if k[0] == j[0] and k[1] != j[1]:
+                        lista = list(j[0])
+                        one = lista[0]
+                        two = lista[1]
+                        # find x/y
+                        # print(one, two)
+                        if one[0] == two[0]:
+                            some_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+                            # print(int(one[1]) in some_numbers, int(one[1]), type(int(one[1])))
+                            some_numbers.remove(int(one[1]))
+                            some_numbers.remove(int(two[1]))
+                            for l in some_numbers:
+                                coord = one[0] + str(l)
+                                if type(self.coords[coord][0]) == set:
+                                    self.coords[coord][0].add(k[1])
+                                    self.coords[coord][0].add(j[1])
+                                    change = True
+        return False
 
 
 
 
 if __name__ == '__main__':
-#     file = """
-# 5  4673 9
-# 9 381 427
-# 1742 3
-# 231976854
-# 857124 9
-# 4963 8172
-#     8926
-# 782641  5
-#  1    7 8
-# """
-#     s = Sudoku(file)
-#     s.solve()
+    file = """
+1   7  3 
+83 6     
+  29  6 8
+6    49 7
+ 9     5 
+3 75    4
+2 3  91  
+     2 43
+ 4  8   9
+"""
 
 #     file = """
 # 53  7
@@ -235,20 +267,7 @@ if __name__ == '__main__':
 #    419  5
 #     8  79
 # """
-#     s = Sudoku(file)
-#     a = s.solve()
 
-
-    file = """
-1   7  3
-83 6
-  29  6 8
-6    49 7
- 9     5
-3 75    4
-2 3  91
-     2 43
- 4  8   9
-"""
     s = Sudoku(file)
+    # print(s.show_board())
     s.solve()
